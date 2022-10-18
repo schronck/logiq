a8b98bc4dc9f118b9df73cd30e0fd47a36711549
PopcornPaws <lipschitz77@gmail.com>
2022-10-18 11:15:31 +0200

    Evaluate tree directly without BDD.

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -6,6 +6,7 @@ use crate::TerminalId;
 use parse::parse;
 pub use parse::ParseError;
 
+use std::collections::HashMap;
 use std::str::FromStr;
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
@@ -31,6 +32,29 @@ impl TokenTree {
     pub fn new(source: &str) -> Result<Self, ParseError> {
         parse(source)
     }
+
+    pub fn evaluate(&self, terminals: &HashMap<TerminalId, bool>) -> Result<bool, String> {
+        let eval;
+        match self {
+            TokenTree::Terminal(c) => {
+                eval = *terminals
+                    .get(c)
+                    .ok_or("Invalid terminals map".to_string())?
+            }
+            TokenTree::Gate { gate, left, right } => {
+                let left_eval = left.evaluate(terminals)?;
+                let right_eval = right.evaluate(terminals)?;
+                match gate {
+                    Gate::And => eval = left_eval && right_eval,
+                    Gate::Or => eval = left_eval || right_eval,
+                    Gate::Nand => eval = !(left_eval && right_eval),
+                    Gate::Nor => eval = !(left_eval || right_eval),
+                    Gate::Xor => eval = left_eval ^ right_eval,
+                }
+            }
+        }
+        Ok(eval)
+    }
 }
 
 impl FromStr for TokenTree {
@@ -38,4 +62,27 @@ impl FromStr for TokenTree {
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         Self::new(input)
     }
+}
+
+#[test]
+fn evaluate_tree() {
+    let tree = TokenTree::from_str("0 AND 1 OR ((0 NAND 2) OR 3)").unwrap();
+    let mut terminals = HashMap::new();
+    terminals.insert(0, true);
+    terminals.insert(1, true);
+    terminals.insert(2, true);
+    terminals.insert(3, true);
+    assert!(tree.evaluate(&terminals).unwrap());
+    let terminal = terminals.get_mut(&1).unwrap();
+    *terminal = false;
+    assert!(tree.evaluate(&terminals).unwrap());
+    let terminal = terminals.get_mut(&3).unwrap();
+    *terminal = false;
+    assert!(!tree.evaluate(&terminals).unwrap());
+    let terminal = terminals.get_mut(&2).unwrap();
+    *terminal = false;
+    assert!(tree.evaluate(&terminals).unwrap());
+    let terminal = terminals.get_mut(&0).unwrap();
+    *terminal = false;
+    assert!(tree.evaluate(&terminals).unwrap());
 }
